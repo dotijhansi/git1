e8fda7743296c587726a6e6429df770897fe23e3
dotijhansi <163412692+dotijhansi@users.noreply.github.com>
2025-08-20 17:08:22 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,4 +1,4 @@
 import java.util.*;
 public static void main(String[] args){
-System.out.println("this is my first program which uses Jenkins"); 
+System.out.println("this is my first program which uses Jenkins pollSCM"); 
 }
